b19e22d321518e56e4d2cb5188121bddf6f8f441
goback-me <adeelrehman662@gmail.com>
2026-08-24 16:15:48 +0500

    fix QuizBuilder type narrowing, remove duplicate folder

--- quiz-os/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs'
-
-export default function Page() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', paddingTop: 80 }}>
-      <SignIn />
-    </div>
-  )
-}
